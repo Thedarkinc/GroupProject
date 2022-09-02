fac6d68447999a42404633da4b506cc6014d878c
Arnold <kgonea@gmail.com>
2022-09-02 21:56:11 +0200

    deleted tests

--- GroupProject/Form1.vb
+++ GroupProject/Form1.vb
@@ -1,13 +1,3 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'FOR PRACTICAL
-        'ooops'
-        Dim A As Integer = 40
-        Dim Auy As Integer = 10
 
-    End Sub
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        'hyyh'
-    End Sub
 End Class
