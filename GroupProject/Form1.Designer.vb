da07b9f1c519d2f57e484f9662ff0a2051d3a9b7
Arnold <kgonea@gmail.com>
2022-09-03 19:24:46 +0200

    Added Menu buttons

--- GroupProject/Form1.Designer.vb
+++ GroupProject/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class frmMenu
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -23,28 +23,82 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(105, 110)
+        Me.Button1.Font = New System.Drawing.Font("Copperplate Gothic Light", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(12, 110)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(81, 30)
+        Me.Button1.Size = New System.Drawing.Size(658, 44)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "button"
+        Me.Button1.Text = "Insert Menu item"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'Form1
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Copperplate Gothic Light", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(12, 160)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(658, 44)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Insert Menu item"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Copperplate Gothic Light", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.Location = New System.Drawing.Point(12, 210)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(658, 44)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Insert Menu item"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Copperplate Gothic Light", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button4.Location = New System.Drawing.Point(12, 260)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(658, 44)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Insert Menu item"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Copperplate Gothic Light", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button5.Location = New System.Drawing.Point(12, 310)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(658, 44)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Insert Menu item"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'frmMenu
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 21.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlText
         Me.ClientSize = New System.Drawing.Size(682, 372)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
-        Me.Name = "Form1"
+        Me.Name = "frmMenu"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
 End Class
