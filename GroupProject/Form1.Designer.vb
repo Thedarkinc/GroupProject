4ebb7c6a808b3c5e4d3f01b6b0a136e9aa5069a5
Arnold <kgonea@gmail.com>
2022-09-03 22:35:47 +0200

    resize

--- GroupProject/Form1.Designer.vb
+++ GroupProject/Form1.Designer.vb
@@ -37,7 +37,7 @@ Partial Class frmMenu
         Me.btnWork.ForeColor = System.Drawing.Color.Black
         Me.btnWork.Image = CType(resources.GetObject("btnWork.Image"), System.Drawing.Image)
         Me.btnWork.ImageAlign = System.Drawing.ContentAlignment.TopCenter
-        Me.btnWork.Location = New System.Drawing.Point(30, 68)
+        Me.btnWork.Location = New System.Drawing.Point(22, 74)
         Me.btnWork.Name = "btnWork"
         Me.btnWork.Size = New System.Drawing.Size(616, 67)
         Me.btnWork.TabIndex = 0
@@ -53,7 +53,7 @@ Partial Class frmMenu
         Me.btnSport.ForeColor = System.Drawing.Color.Black
         Me.btnSport.Image = CType(resources.GetObject("btnSport.Image"), System.Drawing.Image)
         Me.btnSport.ImageAlign = System.Drawing.ContentAlignment.TopCenter
-        Me.btnSport.Location = New System.Drawing.Point(30, 141)
+        Me.btnSport.Location = New System.Drawing.Point(22, 147)
         Me.btnSport.Name = "btnSport"
         Me.btnSport.Size = New System.Drawing.Size(616, 67)
         Me.btnSport.TabIndex = 1
@@ -69,7 +69,7 @@ Partial Class frmMenu
         Me.btnEnt.ForeColor = System.Drawing.Color.Black
         Me.btnEnt.Image = CType(resources.GetObject("btnEnt.Image"), System.Drawing.Image)
         Me.btnEnt.ImageAlign = System.Drawing.ContentAlignment.TopCenter
-        Me.btnEnt.Location = New System.Drawing.Point(30, 214)
+        Me.btnEnt.Location = New System.Drawing.Point(22, 220)
         Me.btnEnt.Name = "btnEnt"
         Me.btnEnt.Size = New System.Drawing.Size(616, 67)
         Me.btnEnt.TabIndex = 2
@@ -86,7 +86,7 @@ Partial Class frmMenu
         Me.Controls.Add(Me.btnSport)
         Me.Controls.Add(Me.btnWork)
         Me.Name = "frmMenu"
-        Me.Text = "Form1"
+        Me.Text = "x"
         Me.ResumeLayout(False)
 
     End Sub
