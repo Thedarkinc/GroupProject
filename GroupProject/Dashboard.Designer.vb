48c27ca65a977f6486b96ba2bd6d1d942533baba
Arnold <kgonea@gmail.com>
2022-09-08 23:02:51 +0200

    added dashboard

---
+++ GroupProject/Dashboard.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Dashboard
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Guna2Button1 = New Guna.UI2.WinForms.Guna2Button()
+        Me.Guna2Button2 = New Guna.UI2.WinForms.Guna2Button()
+        Me.Guna2Button3 = New Guna.UI2.WinForms.Guna2Button()
+        Me.Guna2Separator1 = New Guna.UI2.WinForms.Guna2Separator()
+        Me.Guna2Separator2 = New Guna.UI2.WinForms.Guna2Separator()
+        Me.Guna2Separator3 = New Guna.UI2.WinForms.Guna2Separator()
+        Me.Guna2Separator4 = New Guna.UI2.WinForms.Guna2Separator()
+        Me.Guna2Button4 = New Guna.UI2.WinForms.Guna2Button()
+        Me.Guna2Separator5 = New Guna.UI2.WinForms.Guna2Separator()
+        Me.Guna2GradientPanel1 = New Guna.UI2.WinForms.Guna2GradientPanel()
+        Me.Guna2GradientPanel2 = New Guna.UI2.WinForms.Guna2GradientPanel()
+        Me.Guna2GradientPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Guna2Button1
+        '
+        Me.Guna2Button1.BackColor = System.Drawing.Color.DarkSlateBlue
+        Me.Guna2Button1.BorderRadius = 20
+        Me.Guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.Guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.Guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.Guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.Guna2Button1.FillColor = System.Drawing.Color.Black
+        Me.Guna2Button1.FocusedColor = System.Drawing.Color.MediumOrchid
+        Me.Guna2Button1.Font = New System.Drawing.Font("Perpetua Titling MT", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Guna2Button1.ForeColor = System.Drawing.Color.White
+        Me.Guna2Button1.Location = New System.Drawing.Point(15, 112)
+        Me.Guna2Button1.Name = "Guna2Button1"
+        Me.Guna2Button1.PressedColor = System.Drawing.SystemColors.MenuBar
+        Me.Guna2Button1.Size = New System.Drawing.Size(187, 45)
+        Me.Guna2Button1.TabIndex = 0
+        Me.Guna2Button1.Text = "View Workers"
+        '
+        'Guna2Button2
+        '
+        Me.Guna2Button2.BackColor = System.Drawing.Color.DarkSlateBlue
+        Me.Guna2Button2.BorderRadius = 20
+        Me.Guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.Guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.Guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.Guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.Guna2Button2.FillColor = System.Drawing.Color.Black
+        Me.Guna2Button2.FocusedColor = System.Drawing.Color.MediumPurple
+        Me.Guna2Button2.Font = New System.Drawing.Font("Perpetua Titling MT", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Guna2Button2.ForeColor = System.Drawing.Color.White
+        Me.Guna2Button2.Location = New System.Drawing.Point(12, 173)
+        Me.Guna2Button2.Name = "Guna2Button2"
+        Me.Guna2Button2.PressedColor = System.Drawing.SystemColors.MenuBar
+        Me.Guna2Button2.Size = New System.Drawing.Size(190, 45)
+        Me.Guna2Button2.TabIndex = 1
+        Me.Guna2Button2.Text = "View Most paid"
+        '
+        'Guna2Button3
+        '
+        Me.Guna2Button3.BackColor = System.Drawing.Color.DarkSlateBlue
+        Me.Guna2Button3.BorderRadius = 20
+        Me.Guna2Button3.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.Guna2Button3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.Guna2Button3.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.Guna2Button3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.Guna2Button3.FillColor = System.Drawing.Color.Black
+        Me.Guna2Button3.FocusedColor = System.Drawing.Color.MediumPurple
+        Me.Guna2Button3.Font = New System.Drawing.Font("Perpetua Titling MT", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Guna2Button3.ForeColor = System.Drawing.Color.White
+        Me.Guna2Button3.Location = New System.Drawing.Point(12, 237)
+        Me.Guna2Button3.Name = "Guna2Button3"
+        Me.Guna2Button3.PressedColor = System.Drawing.SystemColors.MenuBar
+        Me.Guna2Button3.Size = New System.Drawing.Size(190, 45)
+        Me.Guna2Button3.TabIndex = 2
+        Me.Guna2Button3.Text = "View Under Paid"
+        '
+        'Guna2Separator1
+        '
+        Me.Guna2Separator1.Location = New System.Drawing.Point(37, 157)
+        Me.Guna2Separator1.Name = "Guna2Separator1"
+        Me.Guna2Separator1.Size = New System.Drawing.Size(117, 10)
+        Me.Guna2Separator1.TabIndex = 3
+        '
+        'Guna2Separator2
+        '
+        Me.Guna2Separator2.Location = New System.Drawing.Point(37, 221)
+        Me.Guna2Separator2.Name = "Guna2Separator2"
+        Me.Guna2Separator2.Size = New System.Drawing.Size(117, 10)
+        Me.Guna2Separator2.TabIndex = 4
+        '
+        'Guna2Separator3
+        '
+        Me.Guna2Separator3.Location = New System.Drawing.Point(37, 288)
+        Me.Guna2Separator3.Name = "Guna2Separator3"
+        Me.Guna2Separator3.Size = New System.Drawing.Size(117, 10)
+        Me.Guna2Separator3.TabIndex = 5
+        '
+        'Guna2Separator4
+        '
+        Me.Guna2Separator4.Location = New System.Drawing.Point(37, 96)
+        Me.Guna2Separator4.Name = "Guna2Separator4"
+        Me.Guna2Separator4.Size = New System.Drawing.Size(117, 10)
+        Me.Guna2Separator4.TabIndex = 6
+        '
+        'Guna2Button4
+        '
+        Me.Guna2Button4.BackColor = System.Drawing.Color.DarkSlateBlue
+        Me.Guna2Button4.BorderRadius = 20
+        Me.Guna2Button4.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.Guna2Button4.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.Guna2Button4.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.Guna2Button4.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.Guna2Button4.FillColor = System.Drawing.Color.Black
+        Me.Guna2Button4.FocusedColor = System.Drawing.Color.MediumPurple
+        Me.Guna2Button4.Font = New System.Drawing.Font("Perpetua Titling MT", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Guna2Button4.ForeColor = System.Drawing.Color.White
+        Me.Guna2Button4.Location = New System.Drawing.Point(12, 304)
+        Me.Guna2Button4.Name = "Guna2Button4"
+        Me.Guna2Button4.PressedColor = System.Drawing.SystemColors.MenuBar
+        Me.Guna2Button4.Size = New System.Drawing.Size(190, 45)
+        Me.Guna2Button4.TabIndex = 7
+        Me.Guna2Button4.Text = "View stats"
+        '
+        'Guna2Separator5
+        '
+        Me.Guna2Separator5.Location = New System.Drawing.Point(37, 355)
+        Me.Guna2Separator5.Name = "Guna2Separator5"
+        Me.Guna2Separator5.Size = New System.Drawing.Size(117, 10)
+        Me.Guna2Separator5.TabIndex = 8
+        '
+        'Guna2GradientPanel1
+        '
+        Me.Guna2GradientPanel1.AutoRoundedCorners = True
+        Me.Guna2GradientPanel1.BackColor = System.Drawing.Color.DarkSlateBlue
+        Me.Guna2GradientPanel1.BorderRadius = 91
+        Me.Guna2GradientPanel1.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2Separator5)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2Button2)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2Separator1)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2Button4)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2Separator2)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2Separator4)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2Button1)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2Button3)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2Separator3)
+        Me.Guna2GradientPanel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Guna2GradientPanel1.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.Vertical
+        Me.Guna2GradientPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.Guna2GradientPanel1.Name = "Guna2GradientPanel1"
+        Me.Guna2GradientPanel1.Size = New System.Drawing.Size(184, 511)
+        Me.Guna2GradientPanel1.TabIndex = 0
+        '
+        'Guna2GradientPanel2
+        '
+        Me.Guna2GradientPanel2.BackColor = System.Drawing.Color.DarkSlateBlue
+        Me.Guna2GradientPanel2.BorderRadius = 10
+        Me.Guna2GradientPanel2.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash
+        Me.Guna2GradientPanel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Guna2GradientPanel2.FillColor = System.Drawing.SystemColors.MenuBar
+        Me.Guna2GradientPanel2.FillColor2 = System.Drawing.Color.Azure
+        Me.Guna2GradientPanel2.Location = New System.Drawing.Point(184, 0)
+        Me.Guna2GradientPanel2.Name = "Guna2GradientPanel2"
+        Me.Guna2GradientPanel2.Size = New System.Drawing.Size(640, 511)
+        Me.Guna2GradientPanel2.TabIndex = 1
+        '
+        'Dashboard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Ivory
+        Me.ClientSize = New System.Drawing.Size(824, 511)
+        Me.Controls.Add(Me.Guna2GradientPanel2)
+        Me.Controls.Add(Me.Guna2GradientPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Dashboard"
+        Me.Text = "Dashboard"
+        Me.Guna2GradientPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Guna2Button1 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Guna2Button2 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Guna2Button3 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Guna2Separator1 As Guna.UI2.WinForms.Guna2Separator
+    Friend WithEvents Guna2Separator2 As Guna.UI2.WinForms.Guna2Separator
+    Friend WithEvents Guna2Separator3 As Guna.UI2.WinForms.Guna2Separator
+    Friend WithEvents Guna2Separator4 As Guna.UI2.WinForms.Guna2Separator
+    Friend WithEvents Guna2Button4 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Guna2Separator5 As Guna.UI2.WinForms.Guna2Separator
+    Friend WithEvents Guna2GradientPanel1 As Guna.UI2.WinForms.Guna2GradientPanel
+    Friend WithEvents Guna2GradientPanel2 As Guna.UI2.WinForms.Guna2GradientPanel
+End Class
--- GroupProject/Form1.vb
+++ GroupProject/Form1.vb
@@ -1,6 +1,15 @@
 ﻿Public Class frmMenu
     Private Sub frmMenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+
+
+    End Sub
+
+    Private Sub btnWork_Click(sender As Object, e As EventArgs) Handles btnWork.Click
+        Dashboard.Show()
+        Me.Hide()
+
+
     End Sub
     'Testing upload
 End Class
