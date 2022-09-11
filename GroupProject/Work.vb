730350fc73a6081c650b0e21fa94056613f2dc6e
Arnold <kgonea@gmail.com>
2022-09-11 03:07:38 +0200

    Added a work class

---
+++ GroupProject/Work.vb
@@ -0,0 +1,38 @@
+﻿Option Strict On
+Option Infer Off
+Option Explicit On
+
+
+Public Class Work
+
+    Private _NumWorkers As Integer 'Number of workers
+    Private _NumWorkdays As Integer 'Number of workdays per month default
+    Private _Manager As String 'Name of manager
+    Private _BonusMargin As Double 'a percentage for bonuses paid to workers
+
+    Public Property NumWorkers() As Integer
+        Get
+            Return _NumWorkers
+        End Get
+        Set(value As Integer)
+
+            If value > 0 Then
+                _NumWorkers = value
+            End If
+        End Set
+    End Property
+    Public Property NumWorkDays() As Integer
+        Get
+            Return _NumWorkdays
+        End Get
+        Set(value As Integer)
+            If value > 0 Then
+                _NumWorkdays = value
+            End If
+        End Set
+    End Property
+
+
+
+
+End Class
