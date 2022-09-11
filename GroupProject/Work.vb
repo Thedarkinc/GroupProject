d937e9f21252d567aaec4d04e2eb119561c9f6dc
Arnold <kgonea@gmail.com>
2022-09-11 04:02:50 +0200

    Added a worker class

--- GroupProject/Work.vb
+++ GroupProject/Work.vb
@@ -9,7 +9,8 @@ Public Class Work
     Private _NumWorkdays As Integer 'Number of workdays per month default
     Private _Manager As String 'Name of manager
     Private _BonusMargin As Double 'a percentage for bonuses paid to workers
-
+    Private _NumFemaleWorkers As Integer 'number of female workers
+    Private _NumMaleworkers As Integer    'number of male workers
     Public Property NumWorkers() As Integer
         Get
             Return _NumWorkers
@@ -44,7 +45,30 @@ Public Class Work
         End Get
         Set(value As Double)
             If value >= 0 Then
-                _BonusMargin
+                _BonusMargin = value
+            Else
+                _BonusMargin = 0
+            End If
+
+        End Set
+    End Property
+    Public Property NumFemaleWorkers() As Integer
+        Get
+            Return _NumFemaleWorkers
+        End Get
+        Set(value As Integer)
+            If value >= 0 Then
+                _NumFemaleWorkers = value
+            End If
+        End Set
+    End Property
+    Public Property NumMaleWorkers() As Integer
+        Get
+            Return _NumMaleworkers
+        End Get
+        Set(value As Integer)
+            If value >= 0 Then
+                _NumMaleworkers = value
             End If
         End Set
     End Property
---
+++ GroupProject/Worker.vb
@@ -0,0 +1,41 @@
+﻿Public Class Worker
+
+    Protected _Name As String
+    Protected _Age As Integer
+    Protected _Salary As Double
+    Protected _JobName As String
+    Protected _YearsExperiance As Integer
+    Protected _AbsentDays As Integer
+
+    Public Property Name() As String
+        Get
+            Return _Name
+        End Get
+        Set(value As String)
+            _Name = value
+        End Set
+    End Property
+
+    Public Property Age() As Integer
+        Get
+            Return _Age
+        End Get
+        Set(value As Integer)
+            _Age = value
+        End Set
+    End Property
+    Public Property Salary() As Double
+        Get
+            Return _Salary
+        End Get
+        Set(value As Double)
+            If value >= 0 Then
+                _Salary = value
+            Else
+                _Salary = 0
+            End If
+        End Set
+    End Property
+
+
+End Class
